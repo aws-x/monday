cbccb78ac56474f77a17b2976129c55bea62d47f
Antonio Pierro <apierr>
2023-02-23 09:52:39 +0100

    first commit

---
+++ monday.js
@@ -0,0 +1,176 @@
+const updateText = async () => {
+    fetch(
+        'https://onedrive.monday.com/onedrive?origin=https://antoniomon.monday.com/boards/1152500419/pulses/1156819881&is_support_multiple=true&is_upload=true',
+        {
+            headers: {
+                accept: 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+                'accept-language': 'en-US,en-GB;q=0.9,en;q=0.8,fr;q=0.7,it;q=0.6',
+                'sec-ch-ua': '"Chromium";v="110", "Not A(Brand";v="24", "Google Chrome";v="110"',
+                'sec-ch-ua-mobile': '?0',
+                'sec-ch-ua-platform': '"macOS"',
+                'sec-fetch-dest': 'iframe',
+                'sec-fetch-mode': 'navigate',
+                'sec-fetch-site': 'same-site',
+                'sec-fetch-user': '?1',
+                'upgrade-insecure-requests': '1',
+            },
+            referrerPolicy: 'same-origin',
+            body: null,
+            method: 'GET',
+            mode: 'cors',
+            credentials: 'include',
+        }
+    );
+    fetch('chrome-extension://kbfnbcaeplbcioakkpcpgfkobkghlhen/src/css/automaticallyAppliedAlert.styles.css', {
+        headers: {
+            'if-modified-since': 'Tue, 14 Feb 2023 10:48:30 GMT',
+            'if-none-match': '"LlKpeLFx4/F2ZM/9zsbqPMxDkb4="',
+        },
+        referrer: '',
+        referrerPolicy: 'same-origin',
+        body: null,
+        method: 'GET',
+        mode: 'cors',
+        credentials: 'omit',
+    });
+    fetch('https://js.live.net/v7.2/OneDrive.js', {
+        referrerPolicy: 'same-origin',
+        body: null,
+        method: 'GET',
+        mode: 'cors',
+        credentials: 'omit',
+    });
+    fetch('https://static.cloudflareinsights.com/beacon.min.js/vaafb692b2aea4879b33c060e79fe94621666317369993', {
+        headers: {
+            'sec-ch-ua': '"Chromium";v="110", "Not A(Brand";v="24", "Google Chrome";v="110"',
+            'sec-ch-ua-mobile': '?0',
+            'sec-ch-ua-platform': '"macOS"',
+        },
+        referrer: '',
+        referrerPolicy: 'same-origin',
+        body: null,
+        method: 'GET',
+        mode: 'cors',
+        credentials: 'omit',
+    });
+    fetch('chrome-extension://kbfnbcaeplbcioakkpcpgfkobkghlhen/src/images/25426cb7a89eb52b/gbtn-progress.svg', {
+        referrer: '',
+        referrerPolicy: 'strict-origin-when-cross-origin',
+        body: null,
+        method: 'GET',
+        mode: 'cors',
+        credentials: 'omit',
+    });
+    fetch('chrome-extension://bfnaelmomeimhlpmgjnjophhpkkoljpa/content_script/inpage_sol.js', {
+        referrer: '',
+        referrerPolicy: 'same-origin',
+        body: null,
+        method: 'GET',
+        mode: 'cors',
+        credentials: 'omit',
+    });
+    fetch('chrome-extension://kbfnbcaeplbcioakkpcpgfkobkghlhen/src/images/3ba4357b2138a763/gbtn-grammarly.svg', {
+        referrer: '',
+        referrerPolicy: 'strict-origin-when-cross-origin',
+        body: null,
+        method: 'GET',
+        mode: 'cors',
+        credentials: 'omit',
+    });
+    fetch(
+        'https://data.eu.pendo.io/data/ptm.gif/46e19b03-1d12-4d5c-6098-f9735b5dbb29?v=2.104.1_prod-eu&ct=1677141574046&jzb=eJztmfFv2joQx_-VyD_jEjckBH6smKrubesmMbauqpCTGPDq2JZt2mZT__deggq0q94DqeFVmpEQ-Gzf985x7oPD5W_kKs3QEM1UvrSogzKjbi0zU8dLMJOk3yc90kv6JI466IZb7pSZ8gJmRIOEkDjpwySa52op3coOI8MkJRHYl0aAYeGctsNul0qnJFelkkfwLmh1lKuymylqCtsFV8dxGPbIoKuXwrLGAm4GaUrAkzZKWzSEcKmZM7f6NgfnJ1_H4_NPMKLRhs9cwDgk2e1UK-umWtCcLZQomIFOx52AtORSCIjaOVP7vO-gsjqTBbtDwxAcLLgoNk1NDZPunWDlRnR0NvlDsV3vj_kcRmV6a6jWba-Yo1nALaa54zesFalmJ-FcSQej1rqNdbqxvqhbR8dXOihEr5PwE-F91pg8lYTm_pKUS2aCdq-oFbxgWFPJxNaqt5jelmCgNJOBUxpn1OCSWUvnDNfGVnJ-nmqzvHsVgzR9qhlBgX1J9OR8dPGoWoLMWmQuVEYFzgyVBZ5BDEFBzTWGXYXdgkH53iGI42dLf7_9gvk0O6t1P-rxqTt_ny1O599jUp2A65mhJWs6J18mF6fV6OdNNKvG3379A51Qvy1XsukeTbSpkvTDkt_94J_jXg2ZyjGIhCRxdN9ZIygXPL_-NwQlsDM8gjyCPII8gjyC_gIEAWUEncOESyTYzKGrQzCpH24x6b-ORcCZN8mkl3aqYQUUOmWwdZVgNli3udz-jknQZI3rtDK4dvtfQ2juevushTN1t4kiE0uzaa3ixWorTKeUqG8y1c6ttQ4vU0V1GBStymSDPlbU2yk4LHA91j3WPdY91g9ysjwAxwf9dPez5SAk8VvnuC85vuT4krN_yfk_DhEkinc9RMR9Eqb-wZZ_sOV_AXsceRz5_1Ze8b-Vqwcyj0Wr',
+        {
+            headers: {
+                'sec-ch-ua': '"Chromium";v="110", "Not A(Brand";v="24", "Google Chrome";v="110"',
+                'sec-ch-ua-mobile': '?0',
+                'sec-ch-ua-platform': '"macOS"',
+            },
+            referrer: '',
+            referrerPolicy: 'same-origin',
+            body: null,
+            method: 'GET',
+            mode: 'cors',
+            credentials: 'omit',
+        }
+    );
+    fetch(
+        'https://data.eu.pendo.io/data/ptm.gif/46e19b03-1d12-4d5c-6098-f9735b5dbb29?v=2.104.1_prod-eu&ct=1677141574048&jzb=eJztl39v2jwQx99K5L9xiaGBwJ8VU9X96iYxtq6qkJMY8OrYlu20ZBPvvZegJrSruiI1PNujICHis3Pfu4t9H3L5C7lcMzRGseDxNeqgyKhby8zc8RTMZDAckmMSDIk_6nfQDbfcKTPnCdzRHw0ICQZDuInGscqk29pJ0PcHIemDPTMCDCvntB13u1Q6JblKlTyCb0Lzo1il3UhRk9guuOoFvn9MRl2dCctKC7gZhSEBT9oobdEYwqVmydz2agnOT75Mp-cfYUWpDb-xgHVIstu5VtbNtaAxWymRMAOTjjsBaclMCIjaOVP43HRQmp_JhK3R2AcHKy6SeqipYdK9ESytRSdns98Um_V-n89hVOa3hmrddMUcjTxuMY0dv2GNSJU7CcdKOlhV6ZbWeW19UreIjm91kI9eJ-EHwvvUmDyUhOH-kpRLZrxmn6gVPGFYU8nETtUbTG9H0FOaSc8pjSNqcMqspUuGC2MjOT9OtSzvXs0gDB9q9qHBPiV6cj65uFdNQaYSWQoVUYEjQ2WCFxCDl1BzjWFXYbdi0L5fEETvUek39aeDFoIu4YZLJNjCoavCBKfirAjkg56euvO30ep0-S0g-QloLQxNWTk5-zy7OM0nP276i3z69ec7mISGbrmS5fRkpk0-CN9nfP2dfwqOC-rkjoES4MbfdComLVSc2WeY1Atgq_yFTHpqpxqWQKNTBluXC2a9aszl7jXueWXWuEgrgme3_zOE4UuPTyUcqXUdRSQyU4-28WK1E6ZTShSHTDVztKrwIpXkh0HRtk2W6GNJsZ28wwK3xXqL9RbrLdYPgPUt2hvm-GgY7nD8D--WvSCAmv2_OV7P7flC-tpwfxbnte3-r1dL95buLd1burd0_1fo_h-8tvd8Mtxc3QGd3jk_',
+        {
+            headers: {
+                'sec-ch-ua': '"Chromium";v="110", "Not A(Brand";v="24", "Google Chrome";v="110"',
+                'sec-ch-ua-mobile': '?0',
+                'sec-ch-ua-platform': '"macOS"',
+            },
+            referrer: '',
+            referrerPolicy: 'same-origin',
+            body: null,
+            method: 'GET',
+            mode: 'cors',
+            credentials: 'omit',
+        }
+    );
+    fetch('https://ei.monday.com/prod/event', {
+        headers: {
+            accept: '*/*',
+            'accept-language': 'en-US,en-GB;q=0.9,en;q=0.8,fr;q=0.7,it;q=0.6',
+            'content-type': 'application/json',
+            'sec-ch-ua': '"Chromium";v="110", "Not A(Brand";v="24", "Google Chrome";v="110"',
+            'sec-ch-ua-mobile': '?0',
+            'sec-ch-ua-platform': '"macOS"',
+            'sec-fetch-dest': 'empty',
+            'sec-fetch-mode': 'cors',
+            'sec-fetch-site': 'same-site',
+        },
+        referrerPolicy: 'same-origin',
+        body: '[{"pulse_account_id":"15306813","pulse_user_id":"39611567","visitor_id":"13c2da2d","account_created_at":"2023-02-12 10:22:36 UTC","pulse_user_serial_number":1,"source":"web_app","os_language":"en-US","user_agent":"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36","browser":"chrome","browser_version":110,"os":"mac","device":"desktop","screen_height":1080,"screen_width":1920,"marketing_referrer":"https://monday.com/","tz_offset":1,"nav_type":0,"info3":"monday","session_id":"f464abdf-138a-379d-e7ff-88003052d988","marketing_landing_page":"https://antoniomon.monday.com/","direct_object_id":1156819881,"board_id":1152500419,"name":"pulse_page_open_editor","timestamp":1677141571,"uuid":"1867d6d761da9c05287a7a3b84171f5256341fa4001867d6d761e1207","seconds_since_happened":8}]',
+        method: 'POST',
+        mode: 'cors',
+        credentials: 'omit',
+    });
+    fetch('https://antoniomon.monday.com/posts', {
+        headers: {
+            accept: 'application/json, text/javascript, */*; q=0.01',
+            'accept-language': 'en-US,en-GB;q=0.9,en;q=0.8,fr;q=0.7,it;q=0.6',
+            'content-type': 'application/x-www-form-urlencoded; charset=UTF-8',
+            'sec-ch-ua': '"Chromium";v="110", "Not A(Brand";v="24", "Google Chrome";v="110"',
+            'sec-ch-ua-mobile': '?0',
+            'sec-ch-ua-platform': '"macOS"',
+            'sec-fetch-dest': 'empty',
+            'sec-fetch-mode': 'cors',
+            'sec-fetch-site': 'same-origin',
+            'x-csrf-token': 'abc750b893c4ed44b9cd801e653518f24057f6ef67242494d9926fdb934478f6',
+            'x-pulse-pusher-socketid': '139148.24682066',
+            'x-requested-with': 'XMLHttpRequest',
+        },
+        referrer: 'https://antoniomon.monday.com/boards/1152500419/pulses/1156819881',
+        referrerPolicy: 'same-origin',
+        body: 'post%5Bitem_id%5D=1156819881&post%5Bkind%5D=update&post%5Bbody%5D=%3Cp%3E%EF%BB%BFciao ciao%3C%2Fp%3E&mentions=%5B%5D&asset_id=%5B%5D&gifs=%5B%5D&version=1&theme=dark',
+        method: 'POST',
+        mode: 'cors',
+        credentials: 'include',
+    });
+    fetch('https://ei.monday.com/prod/event', {
+        headers: {
+            accept: '*/*',
+            'accept-language': 'en-US,en-GB;q=0.9,en;q=0.8,fr;q=0.7,it;q=0.6',
+            'content-type': 'application/json',
+            'sec-ch-ua': '"Chromium";v="110", "Not A(Brand";v="24", "Google Chrome";v="110"',
+            'sec-ch-ua-mobile': '?0',
+            'sec-ch-ua-platform': '"macOS"',
+            'sec-fetch-dest': 'empty',
+            'sec-fetch-mode': 'cors',
+            'sec-fetch-site': 'same-site',
+        },
+        referrerPolicy: 'same-origin',
+        body: '[{"pulse_account_id":"15306813","pulse_user_id":"39611567","visitor_id":"13c2da2d","account_created_at":"2023-02-12 10:22:36 UTC","pulse_user_serial_number":1,"os_language":"en-US","user_agent":"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36","browser":"chrome","browser_version":110,"os":"mac","device":"desktop","screen_height":1080,"screen_width":1920,"marketing_referrer":"https://monday.com/","tz_offset":1,"nav_type":0,"info3":"update","session_id":"f464abdf-138a-379d-e7ff-88003052d988","marketing_landing_page":"https://antoniomon.monday.com/","kind":"post","direct_object":"pulse","direct_object_id":1156819881,"info1":1152500419,"info2":27982863,"board_id":1152500419,"placement":"pulse_page","data":{"item_creator_id":39611567,"post_creator_id":"39611567","is_highlight":false,"workspace_id":-1},"scopes":["pulse_page"],"name":"posted","timestamp":1677141584,"uuid":"1867d6da9171e30496195ddefa4c1f5256341fa4001867d6da918cea","seconds_since_happened":5,"source":"web_app"}]',
+        method: 'POST',
+        mode: 'cors',
+        credentials: 'omit',
+    });
+};
+
+(async (event) => {
+    console.log(`message: ciao`);
+    updateText();
+})({});
+
